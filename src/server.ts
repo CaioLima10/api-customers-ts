f4badd3fa4a20c0938a7ac23b258a7eabf18c0d9
Caio lima <caiohenrys50@gmail.com>
2024-01-02 00:25:44 -0300

    feat: test route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import fastify from "fastify"
+import { routes } from "./routes"
+import cors from "@fastify/cors"
 
 const app = fastify({ logger: true })
 
 const PORT = process.env.PORT || 3333
 
 const start = async () => {
+
+    await app.register(routes)
+    await app.register(cors)
+
     try {
         await app.listen(PORT , () => {
             console.log("running server")
